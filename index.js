2e96bbf26d3427e29305a7ddc8a672013505ced5
Christian Wolfe <cwolfe.k@gmail.com>
2020-07-06 22:49:14 -0400

    questions array added, writeFile function created, generateMarkdown wireframe added

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
-// array of questions for user
-const questions = [
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
+// array of questions for user
+
+
+const questions = [
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the title of your project?"
+
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a brief description of your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Provide brief instruction on installation."
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Please list those already contributing, or, explain how users can contribute to this project."
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please provide a brief test example."
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "What is this product licensed under?"
+    },
+    {
+        type: "input",
+        name: "projectQuestions",
+        message: "Where may contributors or users direct questions regarding this project?"
+    }
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// pass questions array into prompt
+inquirer.prompt(questions)
 
-// function to initialize program
-function init() {
+    // function to initialize generateMarkdown
+    .then(function (data) {
+        const createMarkdown = generateMarkdown(data);
+        fs.writeFile("README.md", createMarkdown, function (err) {
+            if (err) {
+                throw err;
+            }
+            console.log("README.md successfully generated.")
+        });
 
-}
-
-// function call to initialize program
-init();
+    });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,23 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
+## Description
+  ${data.description}
 
+  ## Installation
+  ${data.installation}
+
+  ## Contributors
+  ${data.contributors}
+
+  ## Tests
+  ${data.tests}
+
+  ## License
+  ${data.license}
+
+  ## Questions
+  ${data.userQuestions}
 `;
 }
 
